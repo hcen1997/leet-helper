b3d1f2c008b8336111fdfc25f47c840a4c233c6d
hcen <panzetaoyx@qq.com>
2020-11-25 14:39:30 +0800

    做了好多题呀

--- src/main/java/leetcode/editor/cn/SameStrucString.java
+++ src/main/java/leetcode/editor/cn/SameStrucString.java
@@ -1,4 +1,31 @@
 package leetcode.editor.cn;
 
+import java.util.ArrayList;
+
 public class SameStrucString {
+    public boolean isIsomorphic(String s, String t) {
+        char[] sc = s.toCharArray();
+        char[] tc = t.toCharArray();
+        ArrayList<Character> si = new ArrayList<>(1 << Character.SIZE);
+        ArrayList<Character> ti = new ArrayList<>(1 << Character.SIZE);
+        hashChars(sc, si);
+        hashChars(tc, ti);
+        boolean isI = true;
+        for (int i = 0; i < sc.length; i++) {
+            if( si.indexOf(sc[i])!=ti.indexOf(tc[i])){
+                isI = false;
+                break;
+            }
+        }
+        return isI;
+    }
+
+    private void hashChars(char[] cc, ArrayList<Character> ii) {
+        for (int i = 0; i < cc.length; i++) {
+            Character c = cc[i];
+            if (!ii.contains(c)) {
+                ii.add(c);
+            }
+        }
+    }
 }
