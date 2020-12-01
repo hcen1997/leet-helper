dfde04be8a7f7517cd01a7946a43d9ab32013db8
hcen <panzetaoyx@qq.com>
2020-12-01 17:45:57 +0800

    new monday new code

---
+++ src/main/java/leecode/easy/two/NimGame.java
@@ -0,0 +1,25 @@
+package leecode.easy.two;
+
+public class NimGame {
+
+    /**
+     * me first
+     */
+    public boolean canWinNim(int n) {
+        boolean[] canWin = {
+                true,
+                true,
+                true,
+                false,  // 4
+        };
+        if(n<=4){
+            return canWin[n-1];
+        }else {
+            // n == 4k + 0 => t = 0 => index=3
+            // n == 4k + 1 => t = 1 => index=0
+            // n == 4k + 2 => t = 2 => index=1
+            // n == 4k + 3 => t = 3 => index=2
+            return canWin[((n&3)+3)%4];
+        }
+    }
+}
---
+++ src/test/java/leecode/easy/rand/FriendCirclesTest.java
@@ -0,0 +1,13 @@
+package leecode.easy.rand;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FriendCirclesTest extends FriendCircles {
+
+    @Test
+    public void findCircleNum() {
+        i();
+    }
+}
