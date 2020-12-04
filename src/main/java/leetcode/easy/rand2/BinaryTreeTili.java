ad72477fe11638c0f00e40f4dc0ba1f22bd3e99a
hcen <panzetaoyx@qq.com>
2020-12-04 18:29:49 +0800

    好难

---
+++ src/main/java/leetcode/easy/rand2/BinaryTreeTili.java
@@ -0,0 +1,25 @@
+package leetcode.easy.rand2;
+
+import leetcode.TreeNode;
+
+public class BinaryTreeTili {
+    public int findTilt(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int lt = root.left!=null?val(root.left):0;
+        int lr = root.right!=null?val(root.right):0;
+        int til = Math.abs(lt-lr);
+        return til + findTilt(root.left) + findTilt(root.right);
+    }
+
+    private int val(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        int lt = root.left!=null?val(root.left):0;
+        int lr= root.right!=null?val(root.right):0;
+        return root.val+lr+lt;
+    }
+
+}
---
+++ src/main/java/leetcode/easy/rand2/SlidingWindowMax.java
@@ -0,0 +1,31 @@
+package leetcode.easy.rand2;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class SlidingWindowMax {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (k == 1) {
+            return nums;
+        }
+        if (k == nums.length) {
+            int max = Arrays.stream(nums).max().getAsInt();
+            return new int[]{max};
+        }
+        // 初始化
+        int size = nums.length - k + 1;
+        int[] ans = new int[size];
+        PriorityQueue<Integer> bigger = new PriorityQueue<>(k,Comparator.reverseOrder());
+        for (int i = 0; i < nums.length; i++) {
+            bigger.add(nums[i]);
+            if (i > k - 1) {
+                bigger.remove(nums[i - k]);
+            }
+            if (i >= k - 1) {
+                ans[i - k + 1] = bigger.peek();
+            }
+        }
+        return ans;
+    }
+}
