1b8e7c17990ea589ce8175662d3fe61e06c7526a
hcen <panzetaoyx@qq.com>
2020-12-07 09:58:27 +0800

    new day new thought

--- src/main/java/leetcode/easy/rand2/SlidingWindowMax.java
+++ src/main/java/leetcode/easy/rand2/SlidingWindowMax.java
@@ -5,6 +5,13 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class SlidingWindowMax {
+
+    public int[] maxSlidingWindow1(int[] nums, int k) {
+        // 维护最大数
+        // 最大数如果出去触发堆重建
+        return nums;
+    }
+
     public int[] maxSlidingWindow(int[] nums, int k) {
         if (k == 1) {
             return nums;
@@ -16,7 +23,7 @@ public class SlidingWindowMax {
         // 初始化
         int size = nums.length - k + 1;
         int[] ans = new int[size];
-        PriorityQueue<Integer> bigger = new PriorityQueue<>(k,Comparator.reverseOrder());
+        PriorityQueue<Integer> bigger = new PriorityQueue<>(k, Comparator.reverseOrder());
         for (int i = 0; i < nums.length; i++) {
             bigger.add(nums[i]);
             if (i > k - 1) {
