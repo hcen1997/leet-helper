bd086e9f92af97be5c3ce4a3059c10f3794273f3
hcen1997 <panzetaoyx@gamil.com>
2020-12-30 09:24:30 +0800

    new day new code

---
+++ src/main/java/leetcode/level/easy/rand5/ThreePreorder.java
@@ -0,0 +1,12 @@
+package leetcode.level.easy.rand5;
+
+import leetcode.Node;
+
+import java.util.List;
+
+
+public class ThreePreorder {
+    public List<Integer> preorder(Node root) {
+        return null;
+    }
+}
---
+++ src/main/java/leetcode/level/easy/rand5/ToLowerCase.java
@@ -0,0 +1,22 @@
+package leetcode.level.easy.rand5;
+
+public class ToLowerCase {
+    public String toLowerCase(String str) {
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char aChar = chars[i];
+            if(isUpperCase(aChar)){
+                chars[i]=toLowerCase(aChar);
+            }
+        }
+        return new String(chars);
+    }
+
+    private char toLowerCase(char aChar) {
+        return (char) ('a' + aChar - 'A');
+    }
+
+    private boolean isUpperCase(char aChar) {
+        return aChar>='A'&&aChar<='Z';
+    }
+}
