fa693b426da909c42e8fed384accadfe8027f1da
hcen1997 <panzetaoyx@gamil.com>
2020-12-30 10:58:24 +0800

    add todo

--- src/main/java/leetcode/level/easy/rand5/ThreePreorder.java
+++ src/main/java/leetcode/level/easy/rand5/ThreePreorder.java
@@ -2,11 +2,31 @@ package leetcode.level.easy.rand5;
 
 import leetcode.Node;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 
 
 public class ThreePreorder {
     public List<Integer> preorder(Node root) {
-        return null;
+        List<Integer> ans = new ArrayList<>();
+        if(root==null){
+            return ans;
+        }
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            Node poll = queue.poll();
+            ans.add(poll.val);
+            if (poll.children != null&&poll.children.size()>0) {
+                for (int i = 0; i < poll.children.size(); i++) {
+                    Node node = poll.children.get(i);
+                    queue.offer(node);
+                }
+            }
+            // todo: 中序遍历改为前序遍历
+        }
+        return ans;
     }
 }
