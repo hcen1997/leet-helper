7d9d53f438180643c71cab6a6781c6ea85985287
hcen1997 <panzetaoyx@gamil.com>
2021-03-06 20:13:44 +0800

    add fail test

---
+++ src/test/java/leetcode/level/easy/resume2021/rand1/IsRectangleOverlapTest.java
@@ -0,0 +1,16 @@
+package leetcode.level.easy.resume2021.rand1;
+
+import leetcode.Utils;
+import org.junit.Test;
+
+public class IsRectangleOverlapTest extends IsRectangleOverlap {
+    @Test
+    public void test1() {
+
+        int[] rec1 = Utils.stringToIntegerArray("[2,17,6,20]");
+        int[] rec2 = Utils.stringToIntegerArray("[3,8,6,20]");
+        boolean rectangleOverlap = isRectangleOverlap(rec1, rec2);
+        System.out.println("rectangleOverlap = " + rectangleOverlap);
+    }
+
+}
