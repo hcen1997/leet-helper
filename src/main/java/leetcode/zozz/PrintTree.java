9594445722950bcce5f26774a8c7aa53ff7b3e5c
panzetao <panzetao@gistone.cn>
2021-09-23 09:16:12 +0800

    打印树完成，梦想有一天写出缩减的代码

--- src/main/java/leetcode/zozz/PrintTree.java
+++ src/main/java/leetcode/zozz/PrintTree.java
@@ -34,4 +34,18 @@ public class PrintTree {
             }
         }
     }
+    private void indent(int[][] i) {
+        System.out.println(System.currentTimeMillis());
+        for (int j = 0; j < i.length; j++) {
+            int[] ii = i[j];
+            // 下面这行和46行应该可以合并的 可能需要先处理一下dir数组
+            System.out.println(String.format(fmt, j != i.length - 1 ? vert_left : corner) + ii[0]);
+            if (ii[0] == 0) {
+                String line = j == i.length - 1 ? "    " : String.format(fmt, vert);
+                for (int i1 = 1; i1 < ii.length; i1++) {
+                    System.out.println(line + String.format(fmt, i1 != ii.length - 1 ? vert_left : corner) + ii[i1]);
+                }
+            }
+        }
+    }
 }
