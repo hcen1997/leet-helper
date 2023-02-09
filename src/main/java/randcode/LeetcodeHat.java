0e5a77323537766cb2f9c8d44b79b9e2b93ac91c
hcen1997 <panzetaoyx@qq.com>
2023-02-09 21:46:55 +0800

    只要坚持8个月, 我就是白帽leetcode了

---
+++ src/main/java/randcode/LeetcodeHat.java
@@ -0,0 +1,13 @@
+package randcode;
+
+import org.junit.Test;
+
+public class LeetcodeHat {
+    @Test
+    public void tt() {
+        int coinOneMouth = 30 * 1 + 30 + 30 * 20;
+        System.out.println(coinOneMouth);
+        int mounth = 5400 / coinOneMouth;
+        System.out.println(mounth);
+    }
+}
