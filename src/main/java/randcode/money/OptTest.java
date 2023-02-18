a4f38e37aff82c81402de22983f05eebf7ea067c
hcen1997 <panzetaoyx@qq.com>
2023-02-18 20:54:08 +0800

    一期结论: 总会有人破产的, 为啥倒霉蛋会破产?

--- src/main/java/randcode/money/OptTest.java
+++ src/main/java/randcode/money/OptTest.java
@@ -2,23 +2,29 @@ package randcode.money;
 
 import org.junit.Test;
 
+/**
+ * 随机模拟用户交易程序
+ * todo: 还有两个程序需要重写, 1. 概率查看软件  2. 修仙世界
+ */
 public class OptTest {
     @Test
-    public void t1(){
+    public void t1() {
         System.out.println("这个测试用来模拟10个用户的随机交易");
 
         // 把交易表写在人里面了
         RandOptMan[] market = new RandOptMan[10];
         for (int i = 0; i < 10; i++) {
-            market[i] = new RandOptMan(market,i,100);
+            market[i] = new RandOptMan(market, i, 100);
         }
 
         System.out.println("5000次模拟开始");
         for (int i = 0; i < 5000; i++) {
-            for (RandOptMan man :market){
+            for (RandOptMan man : market) {
                 man.randTrade();
             }
-            printMarketInfo(market);
+            if (i % 50 == 0) {
+                printMarketInfo(market);
+            }
         }
 
         System.out.println("5000次模拟结束");
@@ -27,16 +33,17 @@ public class OptTest {
     }
 
     StringBuilder stmp = new StringBuilder();
+
     private void printMarketInfo(RandOptMan[] market) {
         // [m1,m2,...]
-        stmp.delete(0,stmp.length());
+        stmp.delete(0, stmp.length());
         stmp.append("[");
         for (RandOptMan randOptMan : market) {
-           stmp.append(randOptMan.getMoney());
-           stmp.append(", ");
+            stmp.append(randOptMan.getMoney());
+            stmp.append(", ");
         }
-        if(market.length>0){
-            stmp.deleteCharAt(stmp.length()-1);
+        if (market.length > 0) {
+            stmp.deleteCharAt(stmp.length() - 1);
         }
         stmp.append("]");
         System.out.println(stmp.toString());
