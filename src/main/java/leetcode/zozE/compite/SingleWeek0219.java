754be4766e65c8c72458cde03c9d3241d466dadb
hcen1997 <panzetaoyx@qq.com>
2023-02-19 11:30:12 +0800

    分析后就能过简单题啦

--- src/main/java/leetcode/zozE/compite/SingleWeek0219.java
+++ src/main/java/leetcode/zozE/compite/SingleWeek0219.java
@@ -1,5 +1,7 @@
 package leetcode.zozE.compite;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,6 +34,49 @@ public class SingleWeek0219 {
             }
 
             return ans.toArray(new int[0][0]);
+        }
+    }
+
+    @Test
+    public void t1() {
+        new Solution2().minOperations(39);
+    }
+
+    class Solution2 {
+        public int minOperations(int n) {
+            // 最笨的方法是 直接减  n中每一个bit都减掉对应的2**
+            //System.out.println(Integer.toBinaryString(n));
+
+            String s = Integer.toBinaryString(n);
+            int i = 0;
+            int ans = 0;
+            while ((i = hasConnectOne(s)) > 0) {
+                int add = 1 << s.length() - 1 - i;
+                //System.out.println(add);
+                n += add;
+                ans++;
+                s = Integer.toBinaryString(n);
+            }
+            return ans + countOne(s);
+
+        }
+
+        private int countOne(String s) {
+            int i, ans = 0;
+            for (int i1 = 0; i1 < s.length(); i1++) {
+                ans += s.charAt(i1) == '1' ? 1 : 0;
+            }
+            return ans;
+        }
+
+        private int hasConnectOne(String s) {
+            int i;
+            for (i = s.length() - 1; i > 0; i--) {
+                if (s.charAt(i) == '1' && s.charAt(i - 1) == '1') {
+                    return i;
+                }
+            }
+            return i;
         }
     }
 }
