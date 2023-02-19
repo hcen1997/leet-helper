4fd9329224811e2c37344241d7c6a317538c3b7a
hcen1997 <panzetaoyx@qq.com>
2023-02-19 12:23:51 +0800

    1600选手不能做大于1800的题

--- src/main/java/randcode/LeetcodeHat.java
+++ src/main/java/randcode/LeetcodeHat.java
@@ -15,4 +15,17 @@ public class LeetcodeHat {
         System.out.println(Math.log10(10000)); // 4.0
         System.out.println(Math.log(20000)); // 9.9
     }
+
+    private double rate (int a,int b){
+        double h = (b-a)/400.0;
+        return 1/(1+Math.exp(h* Math.log(10)));
+    }
+    @Test
+    public void pob(){
+        for (int i = 1000; i <=2300; i+=100) {
+            for (int j = 0; j <=2300; j+=100) {
+                System.out.printf("%d %d %.3f\n",i,j,rate(i,j));
+            }
+        }
+    }
 }
